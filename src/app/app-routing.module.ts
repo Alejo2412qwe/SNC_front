7f3997e8071700a6974c9cea9fab7b2a2a77ef01
D4N11EL <101025579+D4N11EL@users.noreply.github.com>
2023-09-13 12:00:59 -0500

    VisualizacionAsistencia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './modulos/sidebar/sidebar.component';
 import { MenuComponent } from './modulos/menu/menu.component';
 import { LoginComponent } from './modulos/login/login.component';
+import { AsistenciaComponent } from './modulos/asistencia/asistencia.component';
 
 const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'sidebar', component: SidebarComponent },
+  { path: 'asis', component: AsistenciaComponent },
 
   { path: '**', redirectTo: 'sidebar' },
 ];
--- src/app/modulos/asistencia/asistencia.component.ts
+++ src/app/modulos/asistencia/asistencia.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllScriptsService } from '../scripts/all-scripts.service';
 
 @Component({
   selector: 'app-asistencia',
   templateUrl: './asistencia.component.html',
   styleUrls: ['./asistencia.component.css']
 })
-export class AsistenciaComponent {
+export class AsistenciaComponent implements OnInit {
+
+  constructor(private AllScripts: AllScriptsService) 
+  {
+    AllScripts.Cargar(["calendario"]);
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
