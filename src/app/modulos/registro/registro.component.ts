e360a97d5b28fbc4658f64be78377e4f440005b2
D4N11EL <dannyandrade989@gmail.com>
2023-10-20 09:16:49 -0500

    VENTANAS FLOTANTES ALEJO

--- src/app/modulos/registro/registro.component.ts
+++ src/app/modulos/registro/registro.component.ts
@@ -352,4 +352,50 @@ export class RegistroComponent implements OnInit {
     validarLetrasNum(event);
   }
 
+  crearProceso() {
+    Swal.fire({
+      title: 'Crear Nuevo Proceso',
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="Proceso o Zona">',
+      showCancelButton: true,
+      confirmButtonText: 'Crear',
+      cancelButtonText: 'Cancelar',
+    });
+  }
+
+  crearSubproceso() {
+    Swal.fire({
+      title: 'Crear Nuevo Subproceso',
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="Subproceso o Departamento">',
+      showCancelButton: true,
+      confirmButtonText: 'Crear',
+      cancelButtonText: 'Cancelar',
+    });
+  }
+
+  crearInstituto() {
+    Swal.fire({
+      title: 'Crear Nuevo Instituto',
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="Instituto">',
+      showCancelButton: true,
+      confirmButtonText: 'Crear',
+      cancelButtonText: 'Cancelar',
+    });
+  }
+
+  
+  crearRol() {
+    Swal.fire({
+      title: 'Crear Nuevo Rol',
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="Rol">',
+      showCancelButton: true,
+      confirmButtonText: 'Crear',
+      cancelButtonText: 'Cancelar',
+    });
+  }
 }
+
+
