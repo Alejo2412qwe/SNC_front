ee552cff1d796838539d35f812df78ce6d0b94ad
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-10-20 08:42:48 -0500

    create subprocesos

--- src/app/services/subprocesos.service.ts
+++ src/app/services/subprocesos.service.ts
@@ -1,15 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { entorno } from '../enviroment/entorno';
 import { Subprocesos } from '../modelo/subprocesos';
+import { SessionStorageService } from './session-storage.service';
 
 export class SuprocesosService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private sessionStorage: SessionStorageService
+  ) {}
 
   public saveSubprocesos(subprocesos: Subprocesos): Observable<Subprocesos> {
+    // Construir el encabezado de autorización con el token JWT
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.sessionStorage.getItem('token')}`, // Agrega el token JWT aquí
+    });
+
     return this.http.post<Subprocesos>(
       entorno.urlPublica + '/subprocesos/create',
-      subprocesos
+      subprocesos,
+      { headers }
     );
   }
 }
