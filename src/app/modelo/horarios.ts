5d151c55293e800a90bdbf5b1c436678efcae88d
SalvadorM1S <salvador.mariscal.est@tecazuay.edu.ec>
2023-10-25 22:46:20 -0500

    cambio

--- src/app/modelo/horarios.ts
+++ src/app/modelo/horarios.ts
@@ -1,40 +1,25 @@
-import { Persona } from "./persona";
-import { Rol } from "./rol";
 
-
-export class Usuario {
-  usuId: number;
-  usuNombreUsuario: string;
-  usuContrasena: string;
-  usuCorreo: string;
-  usuEstado: number;
-  usuFechaRegistro: Date;
-  usuPerId: Persona;
-  rolId: Rol;
-  // insId: Institucion;
-  // funId: Funciones;
+export class Horarios {
+  horId: number;
+  horNumHoras: string;
+  horHoraIngreso: string;
+  horHoraSalida: string;
+  horHoraAlmuerzoInicio: string;
+  horHoraAlmuerzoFin: string;
 
   constructor(
-    usuId?: number,
-    usuNombreUsuario?: string,
-    usuContrasena?: string,
-    usuEstado?: number,
-    usuFechaRegistro?: Date,
-    usuPerId?: Persona,
-    rolId?: Rol,
-    usuCorreo?: string,
-    // insId?:Institucion,
-    // funId?: Funciones,
+    horId?: number,
+    horNumHoras?: string,
+    horHoraIngreso?: string,
+    horHoraSalida?: string,
+    horHoraAlmuerzoInicio?: string,
+    horHoraAlmuerzoFin?: string,
   ) {
-    this.usuId = usuId || 0;
-    this.usuNombreUsuario = usuNombreUsuario || '';
-    this.usuContrasena = usuContrasena || '';
-    this.usuEstado = usuEstado || 0;
-    this.usuFechaRegistro = usuFechaRegistro || new Date();
-    this.usuPerId = usuPerId || new Persona();
-    this.rolId = rolId || new Rol();
-    this.usuCorreo = usuCorreo || '';
-    // this.insId= new Institucion();
-    // this.funId= new Funciones();
+    this.horId = horId || 0;
+    this.horNumHoras = horNumHoras || '';
+    this.horHoraIngreso = horHoraIngreso || '';
+    this.horHoraSalida = horHoraSalida || '';
+    this.horHoraAlmuerzoInicio = horHoraAlmuerzoInicio || '';
+    this.horHoraAlmuerzoFin = horHoraAlmuerzoFin || '';
   }
 }
