1726f5574ad59ece65639948ce75c2c5011a4d5c
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-10-26 16:57:38 -0500

    crud tipo de institucion e institucion completos

--- src/app/modelo/Institucion.ts
+++ src/app/modelo/Institucion.ts
@@ -4,17 +4,20 @@ export class Institucion {
   instId: number;
   instNombre: string;
   instDireccion: string;
+  instEstado: number;
   tipId: TipoInstitucion;
 
   constructor(
     instId?: number,
     instNombre?: string,
     instDireccion?: string,
+    instEstado?: number,
     tipId?: TipoInstitucion
   ) {
     (this.instId = instId || 0),
       (this.instNombre = instNombre || ''),
       (this.instDireccion = instDireccion || ''),
+      (this.instEstado = instEstado || 0),
       (this.tipId = tipId || new TipoInstitucion());
   }
 }
