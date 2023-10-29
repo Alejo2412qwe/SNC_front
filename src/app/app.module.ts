2d8cef798abd79569a5b0cee649626a98b8e3b0a
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-10-29 18:01:34 -0500

    new lista periodos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ListainstitucionesComponent } from './modulos/listainstituciones/listai
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ListaFuncionesComponent } from './modulos/listafunciones/lista-funciones.component';
+import { ListaperiodosComponent } from './modulos/listaperiodos/listaperiodos.component';
 
 @NgModule({
   declarations: [
@@ -39,6 +40,7 @@ import { ListaFuncionesComponent } from './modulos/listafunciones/lista-funcione
     HeaderComponent,
     FooterComponent,
     ListaFuncionesComponent,
+    ListaperiodosComponent,
   ],
   imports: [
     BrowserModule,
