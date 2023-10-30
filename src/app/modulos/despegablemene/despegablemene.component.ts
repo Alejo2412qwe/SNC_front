869f44c2cbd99a17124068ae0528e37d2a90d300
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-10-30 11:25:17 -0500

    cambios

--- src/app/modulos/despegablemene/despegablemene.component.ts
+++ src/app/modulos/despegablemene/despegablemene.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { AllScriptsService } from '../scripts/all-scripts.service';
 import { Router } from '@angular/router';
 import { SessionStorageService } from 'src/app/services/session-storage.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-despegablemene',
@@ -14,13 +15,15 @@ export class DespegablemeneComponent implements OnInit {
     private el: ElementRef,
     private AllScripts: AllScriptsService,
     private router: Router,
-    private sessionStorage: SessionStorageService
+    private sessionStorage: SessionStorageService,
+    private toastr: ToastrService
   ) {}
 
   username = this.sessionStorage.getItem('username');
 
   cerrarSesion(): void {
     localStorage.removeItem('userData');
+    this.toastr.info('Se ha cerrado la sesión');
   }
   ngOnInit(): void {}
 }
