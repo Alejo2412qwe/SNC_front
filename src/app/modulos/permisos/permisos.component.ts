aafcc19614e932e3cf94321d9b8ebdd3f0815d6b
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-11-01 16:19:53 -0500

    cambios

--- src/app/modulos/permisos/permisos.component.ts
+++ src/app/modulos/permisos/permisos.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Permisos } from 'src/app/modelo/permisos';
+import { Provincia } from 'src/app/modelo/provincia';
 import { Regimen } from 'src/app/modelo/regimen';
 import { Usuario } from 'src/app/modelo/usuario';
+import { ProvinciaService } from 'src/app/services/provincia.service';
 import { RegimenService } from 'src/app/services/regimen.service';
 import { SessionStorageService } from 'src/app/services/session-storage.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
@@ -17,8 +19,9 @@ export class PermisosComponent implements OnInit {
     private sessionStorage: SessionStorageService,
     private toastr: ToastrService,
     private regimenService: RegimenService,
-    private usuarioService: UsuarioService
-  ) {}
+    private usuarioService: UsuarioService,
+    private provinciaService: ProvinciaService,
+  ) { }
 
   ngOnInit(): void {
     this.cargarRegimen();
@@ -26,19 +29,28 @@ export class PermisosComponent implements OnInit {
 
   username = this.sessionStorage.getItem('username');
   rol = this.sessionStorage.getItem('rol');
-  idUsuario = this.sessionStorage.getItem('id');
+  idUsuario = this.sessionStorage.getItem('userId');
 
   regimen: Regimen = new Regimen();
   dataUsuario: Usuario = new Usuario();
-  permiso:Permisos = new Permisos();
+  permiso: Permisos = new Permisos();
+  provincia: Provincia = new Provincia();
 
-  cedula:string = '';
+  cedula: string = '';
 
   listaregiemen: Regimen[] = [];
+  listausuario: Usuario[] = [];
+  listProvincias: Provincia[] = [];
 
   cargarRegimen() {
     this.regimenService.getAllRegimen().subscribe((data) => {
       this.listaregiemen = data;
+    });
+  }
+
+  cargarProvincias() {
+    this.provinciaService.getAllProvincias().subscribe((response) => {
+      this.listProvincias = response; // Asigna los datos al array provincias
     });
   }
 
