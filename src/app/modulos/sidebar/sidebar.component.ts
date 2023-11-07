da2668018dc33f4f474001248dc076032392f1c9
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-11-06 22:08:19 -0500

    cambios

--- src/app/modulos/sidebar/sidebar.component.ts
+++ src/app/modulos/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { SessionStorageService } from 'src/app/services/session-storage.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  constructor(
+    private sessionStorage: SessionStorageService,
+    private toastr: ToastrService
+  ) { }
 
+  username = this.sessionStorage.getItem('username');
+  rol = this.sessionStorage.getItem('rol');
 }
