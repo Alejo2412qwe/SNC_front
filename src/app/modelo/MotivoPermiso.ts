79a013b74d7ea296b752d0776689b353bc3f98d7
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-11-10 13:25:31 -0500

    cambio

--- src/app/modelo/MotivoPermiso.ts
+++ src/app/modelo/MotivoPermiso.ts
@@ -2,9 +2,13 @@ export class MotivoPermiso {
   motId: number;
   motNombre: string;
   motEstado: number;
+  motDescripcionLarga: string;
+  motDescripcionCorta: string;
 
-  constructor(motId?: number, motNombre?: string, motEstado?: number) {
+  constructor(motId?: number, motNombre?: string, motEstado?: number, motDescripcionLarga?: string, motDescripcionCorta?: string) {
     (this.motId = motId || 0),
+      (this.motDescripcionLarga = motDescripcionLarga || ''),
+      (this.motDescripcionCorta = motDescripcionCorta || ''),
       (this.motNombre = motNombre || ''),
       (this.motEstado = motEstado || 0);
   }
