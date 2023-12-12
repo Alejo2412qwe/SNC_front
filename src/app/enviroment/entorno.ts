f7de31bfc2b36333516e5821f9b5f148116faf43
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-12-11 19:10:49 -0500

    final para web
    no funciona en local

--- src/app/enviroment/entorno.ts
+++ src/app/enviroment/entorno.ts
@@ -1,5 +1,13 @@
 export const entorno = {
-  urlApi: 'http://localhost:8080/api',
-  urlPublica: 'http://localhost:8080/auth',
-  urlPrivada: 'http://localhost:8080'
+   /*urlApi: 'http://localhost:80/api',
+   urlPublica: 'http://localhost:80/auth',
+   urlPrivada: 'http://localhost:80'*/
+
+  /* urlApi: 'https://dsbackend.tecazuay.edu.ec/snc/api',
+   urlPublica: 'https://dsbackend.tecazuay.edu.ec/snc/auth',
+   urlPrivada: 'https://dsbackend.tecazuay.edu.ec/snc'*/
+
+  urlApi: 'https://apps.tecazuay.edu.ec:9090/snc/api',
+  urlPublica: 'https://apps.tecazuay.edu.ec:9090/snc/auth',
+  urlPrivada: 'https://apps.tecazuay.edu.ec:9090/snc'
 };
