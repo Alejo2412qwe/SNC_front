d83f27636a3b7550d9ad4ba95d9e540a997064ee
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-12-21 14:33:27 -0500

    cambios para web

--- src/app/enviroment/entorno.ts
+++ src/app/enviroment/entorno.ts
@@ -1,18 +1,14 @@
 export const entorno = {
 
-  urlApi: 'http://localhost:8080/snc/api',
+  /*urlApi: 'http://localhost:8080/snc/api',
   urlPublica: 'http://localhost:8080/snc/auth',
   urlPrivada: 'http://localhost:8080/snc'
-
-  /*urlApi: 'http://localhost:80/api',
-  urlPublica: 'http://localhost:80/auth',
-  urlPrivada: 'http://localhost:80'*/
 
   /* urlApi: 'https://dsbackend.tecazuay.edu.ec/snc/api',
    urlPublica: 'https://dsbackend.tecazuay.edu.ec/snc/auth',
    urlPrivada: 'https://dsbackend.tecazuay.edu.ec/snc'*/
 
-  /*urlApi: 'https://apps.tecazuay.edu.ec:9090/snc/api',
+  urlApi: 'https://apps.tecazuay.edu.ec:9090/snc/api',
   urlPublica: 'https://apps.tecazuay.edu.ec:9090/snc/auth',
-  urlPrivada: 'https://apps.tecazuay.edu.ec:9090/snc'*/
+  urlPrivada: 'https://apps.tecazuay.edu.ec:9090/snc'
 };
